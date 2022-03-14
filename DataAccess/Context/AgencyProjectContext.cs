da0ca0733360838027c200c7ff70a4212e67a148
Sinan-Ici <sinanici9@gmail.com>
2022-03-14 15:21:45 +0300

    Add project files.

---
+++ DataAccess/Context/AgencyProjectContext.cs
@@ -0,0 +1,17 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Context
+{
+    public class AgencyProjectContext:DbContext
+    {
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Agency> Agencies { get; set; }
+    }
+}
---
+++ Entities/Agency.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Agency
+    {
+        [Key]
+        public int AgencyID { get; set; }
+        public string AgencyName { get; set; }
+    }
+}
